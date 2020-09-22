c7016c96fac4de12091e8b8ec0034247beece138
Phodal Huang <h@phodal.com>
2020-09-22 12:54:00 +0800

    build: add comments for malloc

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,18 @@ fn main() {
     let long_str = "Boolean";
     let utf_string = NormalString::new(String::from(long_str));
     unsafe {
-        let x = malloc(utf_string.length as usize) as *mut u32;
-        println!("{:?}", *x);
+        let x = malloc(utf_string.length as usize) as *mut u8;
+        println!("{:?}", *x);                  // B
+        println!("{:?}", *x.offset(1)); // o
+        println!("{:?}", *x.offset(2)); // o
+        println!("{:?}", *x.offset(3)); // l
+        println!("{:?}", *x.offset(4)); // e
     }
 
     let long_str2 = "\\b(AbsoluteTime|Boolean|Byte|ByteCount|ByteOffset|BytePtr|CompTimeValue|ConstLogicalAddress|ConstStrFileNameParam|ConstStringPtr|Duration|Fixed|FixedPtr|Float32|Float32Point|Float64|Float80|Float96|FourCharCode|Fract|FractPtr|Handle|ItemCount|LogicalAddress|OptionBits|OSErr|OSStatus|OSType|OSTypePtr|PhysicalAddress|ProcessSerialNumber|ProcessSerialNumberPtr|ProcHandle|Ptr|ResType|ResTypePtr|ShortFixed|ShortFixedPtr|SignedByte|SInt16|SInt32|SInt64|SInt8|Size|StrFileName|StringHandle|StringPtr|TimeBase|TimeRecord|TimeScale|TimeValue|TimeValue64|UInt16|UInt32|UInt64|UInt8|UniChar|UniCharCount|UniCharCountPtr|UniCharPtr|UnicodeScalarValue|UniversalProcHandle|UniversalProcPtr|UnsignedFixed|UnsignedFixedPtr|UnsignedWide|UTF16Char|UTF32Char|UTF8Char)\\b";
     let utf_string2 = NormalString::new(String::from(long_str2));
     unsafe {
-        let x = malloc(utf_string2.length as usize) as *mut u32;
+        let x = malloc(utf_string2.length as usize) as *mut u8;
         println!("{:?}", *x);
     }
 }
